fdc28b1dc2a3742afff1a6209c5387bcd5639810
Vnt0n <antoine.chosson@gmail.com>
2025-02-27 16:17:53 +0100

    Initial Commit

---
+++ MindOpener/MindOpenerApp.swift
@@ -0,0 +1,32 @@
+//
+//  MindOpenerApp.swift
+//  MindOpener
+//
+//  Created by Vnt0n on 27/02/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MindOpenerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
